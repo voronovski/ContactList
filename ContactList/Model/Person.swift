a6e595859a6a8aae7451d563564be7e6f18b0bb4
voronovski <avoronovski@gmail.com>
2023-01-09 23:42:53 -0800

    struct Person and DataStore added

--- ContactList/Model/Person.swift
+++ ContactList/Model/Person.swift
@@ -8,5 +8,29 @@
 struct Person {
     let firstName: String
     let lastName: String
+    let phone: String
     
+    var name: String {
+        "\(firstName) \(lastName)"
+    }
+    
+    var email: String {
+        "\(firstName.first?.lowercased() ?? "")\(lastName.lowercased())@mail.com"
+    }
+    
+    static func getPersons() -> [Person] {
+        var persons: [Person] = []
+        let dataStore = DataStore()
+        
+        for _ in 1...dataStore.firstNames.count {
+            let firstName = dataStore.firstNames.randomElement() ?? "John"
+            let lastName = dataStore.lastNames.randomElement() ?? "Dow"
+            let phone = dataStore.phones.randomElement() ?? "111-222-3344"
+            
+            let person = Person(firstName: firstName, lastName: lastName, phone: phone)
+            persons.append(person)
+        }
+        
+        return persons
+    }
 }
