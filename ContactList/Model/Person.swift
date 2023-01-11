529277a0ea21f323e370c0094d2c79e0245fa737
voronovski <avoronovski@gmail.com>
2023-01-10 23:24:20 -0800

    randomized first + last name + phone matches

--- ContactList/Model/Person.swift
+++ ContactList/Model/Person.swift
@@ -22,9 +22,14 @@ struct Person {
         var persons: [Person] = []
         
         for _ in 1...firstNames.count {
-            let firstName = firstNames.randomElement() ?? "John"
-            let lastName = lastNames.randomElement() ?? "Dow"
-            let phone = phones.randomElement() ?? "111-222-3344"
+            let randomName = Int.random(in: 0..<firstNames.count)
+            let firstName = firstNames.remove(at: randomName)
+            
+            let randomLastName = Int.random(in: 0..<lastNames.count)
+            let lastName = lastNames.remove(at: randomLastName)
+            
+            let randomPhone = Int.random(in: 0..<phones.count)
+            let phone = phones.remove(at: randomPhone)
             
             let person = Person(firstName: firstName, lastName: lastName, phone: phone)
             persons.append(person)
